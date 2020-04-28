000c79df480725e6be4c128a6402bd61bef519b9
Kawhi <wsbpodev001@outlook.com>
2020-04-28 16:30:43 +0800

    heap and bst

---
+++ BST/BST.py
@@ -0,0 +1,288 @@
+"""
+    A BST implementation
+    Last modified at: 2020/4/28
+"""
+
+
+
+class TreeNode(object):
+    """A node in the BST tree."""
+    def __init__(self, key, parent):
+        """Creates a node.
+
+        Args:
+            key: key of the node.
+            parent: The node's parent.
+        
+        Running Time:
+            O(1)
+        """
+        self.key = key
+        self.parent = parent
+        self.left = None
+        self.right = None
+
+    """str method is copied from mit 6.006 material"""
+    def _str(self):
+        """Internal method for ASCII art."""
+        label = str(self.key)
+        if self.left is None:
+            left_lines, left_pos, left_width = [], 0, 0
+        else:
+            left_lines, left_pos, left_width = self.left._str()
+        if self.right is None:
+            right_lines, right_pos, right_width = [], 0, 0
+        else:
+            right_lines, right_pos, right_width = self.right._str()
+        middle = max(right_pos + left_width - left_pos + 1, len(label), 2)
+        pos = left_pos + middle // 2
+        width = left_pos + middle + right_width - right_pos
+        while len(left_lines) < len(right_lines):
+            left_lines.append(' ' * left_width)
+        while len(right_lines) < len(left_lines):
+            right_lines.append(' ' * right_width)
+        if (middle - len(label)) % 2 == 1 and self.parent is not None and \
+           self is self.parent.left and len(label) < middle:
+            label += '.'
+        label = label.center(middle, '.')
+        if label[0] == '.':
+            label = ' ' + label[1:]
+        if label[-1] == '.':
+            label = label[:-1] + ' '
+        lines = [' ' * left_pos + label + ' ' * (right_width - right_pos),
+                 ' ' * left_pos + '/' + ' ' * (middle-2) +
+                 '\\' + ' ' * (right_width - right_pos)] + \
+            [left_line + ' ' * (width - left_width - right_width) + right_line
+             for left_line, right_line in zip(left_lines, right_lines)]
+        return lines, pos, width
+
+    def __str__(self):
+        return '\n'.join(self._str()[0])
+    """str method is copied from mit 6.006 material"""
+
+    def find(self, k):
+        """Finds and returns the node with key k from the subtree rooted at this 
+        node.
+        
+        Args:
+            k: The key of the node we want to find.
+        
+        Returns:
+            The node with key k.
+
+        Running Time:
+            O(log(n))
+        """
+        if k == self.key:
+            return self
+        elif k < self.key:
+            if self.left is None:
+                return None
+            else:
+                return self.left.find(k)
+        else:
+            if self.right is None:  
+                return None
+            else:
+                return self.right.find(k)
+
+    def insert(self, node):
+        """Inserts a node into the subtree rooted at this node.
+        
+        Args:
+            node: The node to be inserted.
+        
+        Running Time:
+            O(log(n))
+        """
+        if(node.key < self.key):
+            if(self.left is None):
+                node.parent = self
+                self.left = node
+            else:
+                node.left.insert(node)
+        else:
+            if(self.right is None):
+                node.parent = self
+                self.right = node
+            else:
+                node.right.insert(node)
+
+    def remove(self):
+        """Removes and returns this node from the BST.
+
+        Running Time:
+            O(log(n))
+        """
+        # wrong first time
+        # swap the key value first instead of swapping the pointer
+        # if the tree is z-zagged or straight:
+        #   just move the children one level up
+        #   there is no need to find next lagger node
+        # else:
+        #   find next lagger node, swap the key
+        # follow course material's idea, remove the root is 
+        # handle by the tree remove with a presudo node
+        if(self.left is None or self.right is None):
+            if(self is self.parent.left):
+                self.parent.left = self.left or self.right
+                if self.parent.left is not None:
+                    self.parent.left.parent = self.parent
+            else:
+                self.parent.right = self.left or self.right
+                if(self.parent.right is not None):
+                    self.parent.right.parent = self.parent
+            return self
+        else:
+            next_larger = self.next_larger()
+            self.key, next_larger.key = next_larger.key, self.key
+            return next_larger.remove()
+                
+    def next_larger(self):
+        """Returns the node with the next larger key (the successor) in the BST.
+
+        Running Time:
+            O(log(n))
+        """
+        if(self.right is None):
+            current = self
+            while(current.parent and current.parent.right == current):
+                current = current.parent
+            return current.parent
+        else:
+            return self.right.find_min()
+    
+    def find_max(self):
+        """Finds the node with the maxmum key in the subtree rooted at this 
+        node.
+        
+        Returns:
+            The node with the maxmum key.
+
+        Running Time:
+            O(log(n))
+        """
+        if(self.right is None):
+            return self
+        else:
+            return self.right.find_max()
+
+    def find_min(self):
+        """Finds the node with the minimum key in the subtree rooted at this 
+        node.
+        
+        Returns:
+            The node with the minimum key.
+
+        Running Time:
+            O(log(n))
+        """
+        if(self.left is None):
+            return self
+        else:
+            return self.left.find_min()
+
+    def check_ri(self):
+        """Checks the BST representation invariant around this node.
+    
+        Assert is not true if the RI is violated.
+
+        Running Time:
+            O(n)
+        """
+        if self.left is not None:
+            assert(self.left.key <= self.key)
+            assert(self.left.parent is self)
+            self.left.check_ri()
+        if self.right is not None:
+            assert(self.right.key >= self.key)
+            assert(self.right.parent is self)
+            self.right.check_ri()
+
+class BST(object):
+    """A binary search tree. Node type is TreeNode"""
+    def __init__(self):
+        """Creates a BST.
+        
+        Running Time:
+            O(1)
+        """
+        self.root = None
+
+    """str method is copied from mit 6.006 material"""
+    def __str__(self):
+        if self.root is None: return '<empty tree>'
+        return str(self.root)
+    """str method is copied from mit 6.006 material"""
+
+    def find(self, k):
+        """Finds and returns the node with key k from the BST.
+        
+        Args:
+            k: The key of the node we want to find.
+        
+        Returns:
+            The node with key k.
+
+        Running Time:
+            O(log(n))
+        """
+        return self.root.find(k)
+
+    def insert(self, key):
+        """Inserts a node into the BST.
+        
+        Args:
+            node: The node to be inserted.
+        
+        Running Time:
+            O(log(n))
+        """
+        if(self.root is None):
+            self.root = TreeNode(key, None)
+        else:
+            self.root.insert(TreeNode(key, None))
+
+    def remove(self, k):
+        """Removes and returns the node with key k from the BST.
+
+        Running Time:
+            O(log(n))
+        """
+        node = self.find(k)
+        if(node is self.root):
+            pseudoroot = TreeNode(0, None)
+            pseudoroot.left = self.root
+            self.root.parent = pseudoroot
+            root = self.root.remove()
+            self.root = pseudoroot.left
+            # wrong first time:
+            # the root may be the only one node
+            # have to check None
+            if self.root is not None:
+                self.root.parent = None
+            return root
+        else:
+            return node.remove()
+
+    def next_larger(self, k):
+        """Returns the node with the next larger key (the successor) in the BST.
+
+        Running Time:
+            O(log(n))
+        """
+        node = self.root.find(k) 
+        if(node is None):
+            return None
+        else:
+            return node.next_larger()
+
+    def check_ri(self):
+        """Checks the BST representation invariant.
+        
+        Raises:
+            An exception if the RI is violated.
+        """
+        if self.root is not None:
+            assert(self.root.parent is None)
+            self.root.__check_ri()
---
+++ Heap/Heap.py
@@ -0,0 +1,195 @@
+"""
+    A Heap/Priority queue implementation
+    Last modified at: 2020/4/28
+"""
+
+class Node:
+    """A node in the Heap."""
+    def __init__(self, value, k):
+        """Creates a node.
+        
+        Args:
+            value: The node's value.
+            k: key of the node.
+        
+        Running Time:
+            O(1)
+        """
+        self.key = k
+        self.val = value
+
+    def __str__(self):
+        return f"{self.__class__.__name__}({self.key}, {self.val})"
+
+
+class Heap:
+    """A Heap"""
+    def __init__(self, values: list):
+        """Creates Heap.
+        
+        Running Time:
+            O(n*log(n))
+        """
+        self.heap = []
+        self._build_heap(values)
+
+    def _build_heap(self, values: list):
+        """Builds the heap from a list.
+        
+        Args:
+            values: The unordered values.
+
+        Running Time:
+            O(n*log(n))
+        """
+        lastIdx = len(values) - 1
+        # start from last nodes that has child
+        startFrom = self._get_parent_idx(lastIdx)
+        for i in values:
+            self.heap.append(i)
+        # check up down
+        for i in range(startFrom, -1, -1):
+            self._heap_down(i)
+        # debug
+        # self.__check_ri()
+
+    def _get_parent_idx(self, idx):
+        # O(1)
+        return (idx - 1) // 2
+
+    def _get_left_child_idx(self, idx):
+        # O(1)
+        return idx * 2 + 1
+
+    def _get_right_child_idx(self, idx):
+        # O(1)
+        return idx * 2 + 2
+
+    def _heap_down(self, idx):
+        # O(log(n))
+        smaller_idx = self.__get_smaller_child_idx(idx)
+        if(not smaller_idx == idx): # if equals, we're done
+            # if not, swap and check next node
+            self.heap[idx], self.heap[smaller_idx] = self.heap[smaller_idx], self.heap[idx]
+            self._heap_down(smaller_idx)
+
+    def _heap_top(self, idx):
+        # O(log(n))
+        parent_idx = self._get_parent_idx(idx)
+        if(parent_idx >= 0 and self.heap[parent_idx].key > self.heap[idx].key): # if not, we're done
+            # if true, swap and check next node
+            self.heap[idx], self.heap[parent_idx] = self.heap[parent_idx], self.heap[idx]
+            self._heap_top(parent_idx)
+                
+
+    # private
+    def __get_smaller_child_idx(self, idx):
+        # O(1)
+        l = self._get_left_child_idx(idx)
+        r = self._get_right_child_idx(idx)
+        smaller_idx = idx
+        if(l < len(self.heap) and self.heap[l].key < self.heap[smaller_idx].key):
+            smaller_idx = l
+        if(r < len(self.heap) and self.heap[r].key < self.heap[smaller_idx].key):
+            smaller_idx = r
+        return smaller_idx
+
+    # public
+    def min(self):
+        """Returns the node with minimum key from the heap.
+
+        Running Time:
+            O(log(n))
+        """
+        if(len(self.heap) > 0):
+            return self.heap[0]
+        else:
+            return None
+
+    def extract_min(self):
+        """Removes and returns the node with minimum key from the heap.
+
+        Running Time:
+            O(log(n))
+        """
+        if(len(self.heap) > 0):
+            # swap first and last
+            self.heap[-1], self.heap[0] = self.heap[0], self.heap[-1]
+            minimum = self.heap.pop()
+            # heapy root
+            self._heap_down(0)
+            # debug
+            # self.__check_ri()
+            # remove last(min) and return it
+            return minimum
+        else:
+            return None
+
+    def insert(self, node):
+        """Inserts a node into the Heap.
+        
+        Args:
+            node: The node to be inserted.
+        
+        Running Time:
+            O(log(n))
+        """
+        self.heap.append(node)
+        # check bottom up
+        self._heap_top(len(self.heap)-1)
+        # debug
+        # self.__check_ri()
+
+    def is_empty(self):
+        """Inserts a node into the Heap.
+        
+        Args:
+            node: The node to be inserted.
+        
+        Returns:
+            Whether the heap is empty.
+
+        Running Time:
+            O(1)
+        """
+        return True if len(self.heap) == 0 else False
+
+    def print_values(self):
+        """Print every node in the Heap.
+
+        Running Time:
+            O(n)
+        """
+        for el in self.heap:
+            print(el)
+    
+    # debugging mathod:
+    # only call when debugging,
+    # check the representation invariant when heap changed
+    def __check_ri(self):
+        # O(n)
+        lastIdx = len(self.heap) - 1
+        # start from last nodes that has child
+        startFrom = self._get_parent_idx(lastIdx)
+        for i in range(startFrom, -1, -1):
+            l = self._get_left_child_idx(i)
+            if(l <= lastIdx):
+                assert(self.heap[i].key <= self.heap[l].key)
+            r = self._get_right_child_idx(i)
+            if(r <= lastIdx):
+                assert(self.heap[i].key <= self.heap[r].key)
+'''
+    test program
+'''
+# if __name__ == "__main__":
+#     h = Heap([Node("四", 4), Node("一", 1), Node("三", 3), Node("二", 2), Node("十六", 16), Node("九", 9), Node("十", 10), Node("十四", 14), Node("八", 8), Node("七", 7)])
+#     # h.print_values()
+#     h.insert(Node("一点五", 1.5))
+#     # print("ex:",h.extract_min())
+#     h.print_values()
+#     print("ex:",h.extract_min())
+#     print("ex:",h.extract_min())
+#     print("ex:",h.extract_min())
+#     print("ex:",h.extract_min())
+#     print("ex:",h.extract_min())
+#     print("ex:",h.extract_min())
